e822e2527113eb3c1bd6f8aa59df2362923c2605
Ademola Abdulkareem <dr.white004@gmail.com>
2025-04-30 11:41:44 +0100

    admin update

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import UserProfile, Portfolio
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'position', 'years_of_experience', 'projects_completed', 'location')
+    search_fields = ('user__username', 'position', 'location')
+    list_filter = ('position', 'location')
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'project_type', 'client', 'duration', 'project_link')
+    search_fields = ('title', 'owner__username', 'client', 'project_type')
+    list_filter = ('project_type', 'client', 'tools')
+    prepopulated_fields = {'slug': ('title',)}  # Auto-fill slug from title
